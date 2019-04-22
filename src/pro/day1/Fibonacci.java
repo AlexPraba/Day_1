cd2b5cc248f4e599ae2e8543bb7ff4ce2ec504eb
Alexis Prabahaaran <alexisprabaharan@gmail.com>
2019-04-22 20:28:58 +0530

    Local to Remote

---
+++ src/pro/day1/Fibonacci.java
@@ -0,0 +1,16 @@
+package pro.day1;
+
+public class Fibonacci {
+public static void main(String[] args) {
+	int a=1;
+	int b=2;
+	int c;
+	
+	for (int i=1; i<=7; i++){
+		System.out.println(a+"");
+	c=a+b;
+	a=b;
+	b=c;
+	}
+}
+}
